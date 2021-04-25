6537bcd6fe7500ea22fd28cfbbfd8a37b4382a06
dburke86 <daniel.burke09@gmail.com>
2021-04-25 17:43:28 -0500

    Add project files.

---
+++ ClassTracker_CS/Course.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTracker_CS
+{
+    class Course
+    {
+        //private variables
+        private string courseTitle;
+        private int creditHours;
+        private string program;
+
+        //public variables
+        public string _courseTitle
+        {
+            get { return courseTitle; }
+            set { courseTitle = value; }
+        }
+
+        public int _creditHours
+        {
+            get { return creditHours; }
+            set { creditHours = value; }
+        }
+
+        public string _program
+        {
+            get { return program; }
+            set { program = value; }
+        }
+        //public methods
+        public void EnrollStudent(string studentName)
+        {
+
+        }
+
+        public virtual float CalculateAverage(float[] arrGrades)
+        {
+            float avg = 0;
+            return avg;
+        }
+    }
+}
---
+++ ClassTracker_CS/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTracker_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCourse credCourse = new CreditCourse();
+
+            credCourse._courseTitle = "CS101";
+            credCourse._creditHours = 6;
+            credCourse._program = "Computer Science";
+            credCourse.EnrollStudent("Danny Burke");
+            float GPA = credCourse.CalculateGPA(98.5F);
+            Console.WriteLine("Danny Burke has a GPA of {0} in course {1}.", GPA, credCourse._courseTitle);
+
+            NonCreditCourse nonCredCrse = new NonCreditCourse();
+            nonCredCrse._courseTitle = "Introduction to Excel";
+            
+            char grade = nonCredCrse.CalculateGrade(87.0F);
+
+            Console.WriteLine("Danny also received a {0} in {1}.", grade, nonCredCrse._courseTitle);
+
+            CreditCourse newCredCourse = new CreditCourse();
+            Console.WriteLine(newCredCourse._courseTitle);
+
+            CreditCourse newCredCourseTwo = new CreditCourse("CS102", 6, "Computer Science");
+            Console.WriteLine(newCredCourseTwo._courseTitle);
+        }
+    }
+}
